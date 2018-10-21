94e78b8c5f8667fa538fd3d4bd0d392418d5a580
Jurrien Fakkeldij <jurrien.fakkeldij@gmail.com>
2018-10-21 23:39:01 +0200

    Added validation for records.

--- src/main/java/com/acme/processor/StatementProcessor.java
+++ src/main/java/com/acme/processor/StatementProcessor.java
@@ -1,11 +1,15 @@
 package com.acme.processor;
 
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import com.acme.model.ProcessingState;
 import com.acme.model.Statement;
-import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.batch.item.ItemProcessor;
 
-import java.util.ArrayList;
-import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Class to process incoming statement and return the output statement.
@@ -13,24 +17,30 @@ import java.util.List;
 @Slf4j
 public class StatementProcessor implements ItemProcessor<Statement, Statement> {
 
+    @Resource
     private List<Statement> processedStatements;
-    private List<Statement> errorStatements;
-
-    /**
-     * Constructor to initialize all the local variables.
-     */
-    public StatementProcessor() {
-        processedStatements =  new ArrayList<>();
-        errorStatements = new ArrayList<>();
-    }
 
     @Override
     public Statement process(final Statement statement) {
 
         log.info("Processing statements.");
+        statement.setState(ProcessingState.CORRECT); //Start with assuming there is no issue.
 
-        
+        for(Statement processedStatement : processedStatements) {
+            if(processedStatement.getReference().compareTo(statement.getReference()) == 0) {
+                //If the references are the same, set both to be at fault.
+                statement.setState(ProcessingState.FAULT_DUPLICATE_REFERENCE);
+                processedStatement.setState(ProcessingState.FAULT_DUPLICATE_REFERENCE);
+            }
+        }
 
-        return new Statement();
+        if(statement.getStartBalance().add(statement.getMutation()).compareTo(statement.getEndBalance()) != 0) {
+            //if the end balance is not correct set to the correct error state.
+            statement.setState(ProcessingState.FAULT_WRONG_END_BALANCE);
+        }
+
+        processedStatements.add(statement);
+
+        return statement;
     }
 }
